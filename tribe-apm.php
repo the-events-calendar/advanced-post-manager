e7e130ce2ac01fda7e94f2da309eb6412968a328
Matthew Batchelder <borkweb@gmail.com>
2015-08-07 10:43:56 -0400

    Upping version number

--- tribe-apm.php
+++ tribe-apm.php
@@ -2,7 +2,7 @@
 /*
  Plugin Name:  Advanced Post Manager
  Description:  Dialing custom post types to 11 with advanced filtering controls.
- Version: 3.10
+ Version: 3.11
  Author: Modern Tribe, Inc.
  Author URI: http://m.tri.be/4n
  Text Domain: tribe-apm
