06e753860cce46a8e42d8c0ddd88aa3c918dce86
bordoni <bordoni.dev@gmail.com>
2016-06-30 14:00:16 -0300

    Bump to version 4.2.2

--- tribe-apm.php
+++ tribe-apm.php
@@ -2,7 +2,7 @@
 /*
  Plugin Name:  Advanced Post Manager
  Description:  Dialing custom post types to 11 with advanced filtering controls.
- Version: 4.2
+ Version: 4.2.2
  Author: Modern Tribe, Inc.
  Author URI: http://m.tri.be/4n
  Text Domain: tribe-apm
