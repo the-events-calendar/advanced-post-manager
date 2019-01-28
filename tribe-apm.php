0e99574debc223be3c22da430e94898616c60dc0
Juanfra Aldasoro <juanfraa@gmail.com>
2019-01-28 18:08:31 -0300

    Include PHP min version code

--- tribe-apm.php
+++ tribe-apm.php
@@ -12,6 +12,35 @@ define( 'TRIBE_APM_PATH', plugin_dir_path( __FILE__ ) );
 define( 'TRIBE_APM_FILE', __FILE__ );
 define( 'TRIBE_APM_LIB_PATH', TRIBE_APM_PATH . 'lib/' );
 
+// Load the required php min version functions
+require_once TRIBE_APM_LIB_PATH . 'php-min-version.php';
+
+/**
+ * Verifies if we need to warn the user about min PHP version and bail to avoid fatals
+ */
+if ( tribe_is_not_min_php_version() ) {
+	tribe_not_php_version_textdomain( 'advanced-post-manager', TRIBE_APM_FILE );
+	/**
+	 * Include the plugin name into the correct place
+	 *
+	 * @since  TBD
+	 *
+	 * @param  array $names current list of names
+	 *
+	 * @return array
+	 */
+	function tribe_apm_not_php_version_plugin_name( $names ) {
+		$names['tribe-apm'] = esc_html__( 'Advanced Post Manager', 'advanced-post-manager' );
+		return $names;
+	}
+	add_filter( 'tribe_not_php_version_names', 'tribe_apm_not_php_version_plugin_name' );
+	if ( ! has_filter( 'admin_notices', 'tribe_not_php_version_notice' ) ) {
+		add_action( 'admin_notices', 'tribe_not_php_version_notice' );
+	}
+	return false;
+}
+
+
 class Tribe_APM {
 	/**
 	 * The current version of iCal Importer
@@ -189,7 +218,7 @@ class Tribe_APM {
 	}
 
 	protected function is_active() {
-		$desired_screen = 'edit-'.$this->post_type;
+		$desired_screen = 'edit-' . $this->post_type;
 
 		// Exit early on autosave
 		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
